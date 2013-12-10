90293d295770a00525dc2bf2c833d33f1dc57bac
quake0day <quake0day@gmail.com>
2013-12-09 22:59:54 -0500

    add dots_p

--- matrix_dotp.py
+++ matrix_dotp.py
@@ -24,6 +24,49 @@ def pprint(string, comm=MPI.COMM_WORLD):
     if comm.rank == 0:
         print(string)
 
+def dots_p(my_A,my_B,comm=MPI.COMM_WORLD):
+    NORTH = 0
+    SOUTH = 1
+    EAST = 2
+    WEST = 3
+    mpi_rows = int(np.floor(np.sqrt(comm.size)))
+    mpi_cols = comm.size // mpi_rows
+    if mpi_rows*mpi_cols > comm.size:
+        mpi_cols -= 1
+    if mpi_rows*mpi_cols > comm.size:
+        mpi_rows -= 1
+
+    ccomm = comm.Create_cart( (mpi_rows, mpi_cols), periods=(True, True), reorder=True)
+
+    my_mpi_row, my_mpi_col = ccomm.Get_coords( ccomm.rank ) 
+    neigh = [0,0,0,0]
+    
+    neigh[NORTH], neigh[SOUTH] = ccomm.Shift(0, 1)
+    neigh[EAST],  neigh[WEST]  = ccomm.Shift(1, 1)
+
+    my_C = np.zeros_like(my_A)
+    tile_A = my_A
+    tile_B = my_B
+    tile_A_ = np.empty_like(my_A)
+    tile_B_ = np.empty_like(my_A)
+    req = [None, None, None, None]
+
+    t0 = time()
+    for r in xrange(mpi_rows):
+        req[EAST]  = ccomm.Isend(tile_A , neigh[EAST])
+        req[WEST]  = ccomm.Irecv(tile_A_, neigh[WEST])
+        req[SOUTH] = ccomm.Isend(tile_B , neigh[SOUTH])
+        req[NORTH] = ccomm.Irecv(tile_B_, neigh[NORTH])
+        #t0 = time()
+        my_C = np.dot(tile_A, tile_B)
+        #t1 = time()
+
+        req[0].Waitall(req)
+        #t2 = time()
+        #print("Time computing %6.2f  %6.2f" % (t1-t0, t2-t1))
+    comm.barrier()
+    t_total = time()-t0
+    return my_C
 
 if __name__ == "__main__":
     comm = MPI.COMM_WORLD
