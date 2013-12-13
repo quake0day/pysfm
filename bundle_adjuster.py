a9a5a4856b2c189aa39b7d7cc5b39907c1de4653
quake0day <quake0day@gmail.com>
2013-12-13 14:30:12 -0500

    test max

--- bundle_adjuster.py
+++ bundle_adjuster.py
@@ -250,7 +250,7 @@ class BundleAdjuster(object):
                     #cuda.memcpy_htod(b_gpu,Jp_float)
                     a_gpu = gpuarray.to_gpu(Jc_float)
                     b_gpu = gpuarray.to_gpu(Jc_T_float)
-                    dot_gpu = gpuarray.dot(b_gpu,a_gpu)
+                    dot_gpu = gpuarray.maximum(b_gpu,a_gpu)
                     print "===="
                     print dot_gpu
                     print "===="
